c86033a3a9d6bc6654a94bc0729db531b717b458
Jia Wei Kho <jwk33@cam.ac.uk>
2019-10-26 20:05:31 +0100

    Added waypoints into directions function

--- utils/google_maps.py
+++ utils/google_maps.py
@@ -1,14 +1,22 @@
 import requests
 import json
 from GPS import get_coords
-
-def directions(destinationCoordinates):
-    originCoordinates = get_coords()
+waypoints = [(52.201156,0.114311),(52.201925, 0.115470)]
+def directions(destinationCoordinates,waypoints):
+    waypoints_list = []
+    separator = '%7C'
+    for i in waypoints:
+        waypoints_list.append(str(i[0])+'%2C'+str(i[1]))
+    print(waypoints_list)
+    apiKey = "key=AIzaSyDYZzLkcTStJFATSjN2ZHotAucE2Z4q8Yc"
+    #originCoordinates = get_coords()
+    originCoordinates = [52.200591,0.113023]
     origin = "origin=" + str(originCoordinates[0]) + ',' + str(originCoordinates[1])
     destination = "destination=" + str(destinationCoordinates[0]) + ',' + str(destinationCoordinates[1])
-
-    apiKey = "key=AIzaSyDYZzLkcTStJFATSjN2ZHotAucE2Z4q8Yc"
-    url = "https://maps.googleapis.com/maps/api/directions/json?" + origin + '&' + destination + '&' + apiKey
+    waypoints = 'waypoints='+  separator.join(waypoints_list)
+    print(waypoints)
+    url = "https://maps.googleapis.com/maps/api/directions/json?" + origin + '&' + destination + '&' + waypoints + '&'  + apiKey 
+    print(url)
 
     try:
         getRequest = requests.get(url)
@@ -19,4 +27,4 @@ def directions(destinationCoordinates):
         print("Exception occured")
 
 if __name__ == "__main__":
-    directions([52.200677, 0.113066])
+    directions([52.202333, 0.117272],waypoints)
