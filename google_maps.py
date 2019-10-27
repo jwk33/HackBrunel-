6a0b852d927ca4f1148d95a9bcb8528e51eb816e
Jia Wei Kho <jwk33@cam.ac.uk>
2019-10-27 01:20:53 +0100

    Fixed Directions on front end

--- google_maps.py
+++ google_maps.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from GPS import get_coords, convert_to_coords
+from GPS import get_coords, convert_to_coords, get_location
 waypoints = [(52.201156,0.114311),(52.201925, 0.115470)]
 def directions(destinationCoordinates,waypoints):
     waypoints_list = []
@@ -32,27 +32,27 @@ def directions(destinationCoordinates,waypoints):
 def directions_map(destinationCoordinates):
     
     apiKey = "key=AIzaSyDYZzLkcTStJFATSjN2ZHotAucE2Z4q8Yc"
-    originCoordinates = get_coords()
+    originCoordinates = get_location()
     origin = "origin=" + str(originCoordinates[0]) + ',' + str(originCoordinates[1])
     destination = "destination=" + str(destinationCoordinates[0]) + ',' + str(destinationCoordinates[1])
     url = "https://www.google.com/maps/embed/v1/directions?" + origin + '&' + destination + '&'  + apiKey 
     return url
 
-def directions_places(place):
+def directions_place(place):
     apiKey = "key=AIzaSyDYZzLkcTStJFATSjN2ZHotAucE2Z4q8Yc"
-    originCoordinates = get_coords()
-    origin = "origin=" + str(originCoordinates[0]) + ',' + str(originCoordinates[1])
-    destination = "destination=" + place
+    input = "input=" + place
     fields_place = 'fields=geometry/location'
     try:
-        input = 'input=name'
         inputType = 'inputtype=textquery'
-        place_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?" + destination + '&'  + input + '&'  + inputType + '&' + fields_place +'&'  + apiKey 
+        place_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?" + input + '&'  + inputType + '&' + fields_place +'&'  + apiKey 
         place_data = requests.get(place_url).json()
-        print(place_data)
+        destinationCoordinates = [place_data["candidates"][0]["geometry"]["location"]["lat"],place_data["candidates"][0]["geometry"]["location"]["lng"]]
+        print(destinationCoordinates)
         
     except:
         print('Error')
+    url_embed = directions_map(destinationCoordinates)
+    return url_embed
 
 def static_map_image(place):
     url = "https://maps.googleapis.com/maps/api/staticmap?"
@@ -65,7 +65,7 @@ def static_map_image(place):
     f.close()
 
 if __name__ == "__main__":
-    directions([52.202333, 0.117272],waypoints)
-    directions_places('Queens\' College')
-    static_map_image("Dockett Building, Queens College, Cambridge")
+    #directions([52.202333, 0.117272],waypoints)
+    directions_place('Queens\' College')
+    #static_map_image("Dockett Building, Queens College, Cambridge")
     
